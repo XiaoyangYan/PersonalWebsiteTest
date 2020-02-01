2ce7304e3a64c50b57654b4df2a36bb58964cba4
Xiaoyang <xiaoyang@propic.com.au>
2020-02-01 21:51:50 +0800

    add all

---
+++ build/precache-manifest.ae400d26fd5714944316f9667b7d6f48.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4689541c410c3f0bcb05d503f2a82474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ea1afc830e135c62d59",
+    "url": "/static/css/2.3fad095e.chunk.css"
+  },
+  {
+    "revision": "63e71d0e59029f295c09",
+    "url": "/static/css/main.6e92ce27.chunk.css"
+  },
+  {
+    "revision": "2ea1afc830e135c62d59",
+    "url": "/static/js/2.a889211f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a889211f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63e71d0e59029f295c09",
+    "url": "/static/js/main.72f86de4.chunk.js"
+  },
+  {
+    "revision": "a6a9d6835b70c814de40",
+    "url": "/static/js/runtime-main.395892aa.js"
+  },
+  {
+    "revision": "77f316be212f1c7dc4409e0c5446d375",
+    "url": "/static/media/banner_1.77f316be.jpg"
+  },
+  {
+    "revision": "5bc6413efc40c7486866f60c8622f836",
+    "url": "/static/media/banner_2.5bc6413e.jpg"
+  },
+  {
+    "revision": "eaccd3daba9c47e6f9022add1c71c4e7",
+    "url": "/static/media/banner_3.eaccd3da.jpg"
+  },
+  {
+    "revision": "cd9c30ea8f57e659382d95d13cfe20b2",
+    "url": "/static/media/banner_4.cd9c30ea.jpg"
+  },
+  {
+    "revision": "2090cd6c906b849bd8e4672eb41dc649",
+    "url": "/static/media/banner_5.2090cd6c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "47b3dbf1c3de7559cb167ef99986227c",
+    "url": "/static/media/slide_1.47b3dbf1.jpg"
+  },
+  {
+    "revision": "f727e28f4c55d83da2087ed582a1a775",
+    "url": "/static/media/slide_1.f727e28f.jpg"
+  },
+  {
+    "revision": "0f85690943efb3296ee23adf565dbe42",
+    "url": "/static/media/slide_2.0f856909.jpg"
+  },
+  {
+    "revision": "cd6e342e512a11707e129671a1396cbc",
+    "url": "/static/media/slide_2.cd6e342e.jpg"
+  },
+  {
+    "revision": "4fedf8354493ce3c2786abc760bca57d",
+    "url": "/static/media/slide_3.4fedf835.jpg"
+  },
+  {
+    "revision": "8fc0655bbd6ee75dee622f4ad6af11d1",
+    "url": "/static/media/slide_3.8fc0655b.jpg"
+  },
+  {
+    "revision": "aac08136b6be1ce61c51af379095adc7",
+    "url": "/static/media/slide_4.aac08136.jpg"
+  }
+]);
